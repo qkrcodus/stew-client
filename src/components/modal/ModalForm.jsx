15c1ba7c4a4b4971009e102b106f1b7c7959d17d
qkrcodus <130819578+qkrcodus@users.noreply.github.com>
2024-08-05 15:35:09 +0900

    feat(api): application-controller post요청 연동

--- src/components/modal/ModalForm.jsx
+++ src/components/modal/ModalForm.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import stars from '../../assets/images/star-fill.png';
 import ReactSlider from 'react-slider';
-import { useState } from 'react';
+import axios from 'axios';
+
 const Overlay = styled.div`
   display: ${(props) => (props.isOpen ? 'block' : 'none')};
   position: fixed;
@@ -20,13 +21,13 @@ const ModalContainer = styled.div`
   width: 100vw;
   height: 100vh;
   .signupformbtn{
-  font-family: var(--font-family-pretendard);
-font-size: 28px;
-font-style: normal;
-font-weight: 500;
-line-height: normal;
-border: none;
-border-radius: 3rem;
+    font-family: var(--font-family-pretendard);
+    font-size: 28px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+    border: none;
+    border-radius: 3rem;
   }
 `;
 
@@ -67,13 +68,13 @@ const TutorThumbnail = styled.div`
 `;
 
 const TutorImg = styled.div`
- background-image: url(${(props) => props.$imgurl});
+  background-image: url(${(props) => props.$imgurl});
   background-size: cover;
-    background-position: center;
+  background-position: center;
   background-repeat: no-repeat;
-      width: 9.7rem;
-    height: 10.7rem;
-    flex-shrink: 0;
+  width: 9.7rem;
+  height: 10.7rem;
+  flex-shrink: 0;
 `;
 
 const RatingImage = styled.img`
@@ -127,6 +128,16 @@ const InfoRow = styled.div`
 `;
 
 const ModalTitle = styled.div`
+input{
+        color: #A6A6A6;
+    font-family: var(--font-family-pretendard);
+    font-size: 2.8rem;
+    border: none;
+
+  &:focus {
+    outline: none;
+  }
+}
   display: flex;
   box-sizing: border-box;
   padding-left: 3.8rem;
@@ -222,7 +233,7 @@ const IntensityPreference = styled.div`
     box-sizing: border-box;
     resize: none;
     border: none;
-    color: var(--Sub-Color, #333E5E);
+    color: var (--Sub-Color, #333E5E);
     font-family: var(--font-family-pretendard);
     font-size: 2.4rem;
     font-style: normal;
@@ -295,72 +306,44 @@ const MessageToTutor = styled.div`
     align-items: center;
   }
 `;
-const Btns=styled.div`
-position : absolute;
-display: flex;
-gap: 2rem;
-top: 3.5rem;
-left: 5rem;
-cursor: pointer;
-`
-const Diet=styled.div`
-color: var(--Main-Color, #6BA6FF);
-font-family: var(--font-family-pretendard);
-font-size: 2.8rem;
-font-style: normal;
-font-weight: 500;
-line-height: normal;
-border-radius: 3rem;
-border: 0.2rem solid var(--Main-Color, #6BA6FF);
-display: inline-flex;
-padding: 0.45rem 2.4rem;
-justify-content: center;
-align-items: center;
-`
 
-const Muscle=styled.div`
-color: var(--Main-Color, #6BA6FF);
-font-family: var(--font-family-pretendard);
-font-size: 2.8rem;
-font-style: normal;
-font-weight: 500;
-line-height: normal;
-border-radius: 3rem;
-border: 0.2rem solid var(--Main-Color, #6BA6FF);
-display: inline-flex;
-padding: 0.45rem 2.4rem;
-justify-content: center;
-align-items: center;
-`
-const Stress=styled.div`
-color: var(--Main-Color, #6BA6FF);
-font-family: var(--font-family-pretendard);
-font-size: 2.8rem;
-font-style: normal;
-font-weight: 500;
-line-height: normal;
-border-radius: 3rem;
-border: 0.2rem solid var(--Main-Color, #6BA6FF);
-display: inline-flex;
-padding: 0.45rem 2.4rem;
-justify-content: center;
-align-items: center;
-`
-const Hobby=styled.div`
-color: var(--Main-Color, #6BA6FF);
-font-family: var(--font-family-pretendard);
-font-size: 2.8rem;
-font-style: normal;
-font-weight: 500;
-line-height: normal;
-border-radius: 3rem;
-border: 0.2rem solid var(--Main-Color, #6BA6FF);
-display: inline-flex;
-padding: 0.45rem 2.4rem;
-justify-content: center;
-align-items: center;
-`
+const Btns = styled.div`
+  position: absolute;
+  display: flex;
+  gap: 2rem;
+  top: 3.5rem;
+  left: 5.5rem;
+  cursor: pointer;
+`;
 
+const PurposeButton = styled.div`
+  background-color: ${({ isSelected }) => (isSelected ? '#DAE9FF' : 'white')};
+  border: 0.2rem solid #6BA6FF;
+  border-radius: 3rem;
+  padding: 0.45rem 1.65rem;
+  justify-content: center;
+align-items: center;
+  margin: 0.5rem;
+  cursor: pointer;
+  display: flex;
+  font-size: 1.2rem;
+  color: ${({ isSelected }) => (isSelected ? '#333E5E' : '#6BA6FF')};
+  &:hover {
+    background-color: #DAE9FF;
+    color: #333E5E;
+  }
+font-family: var(--font-family-pretendard);
+font-size: 2.8rem;
+font-style: normal;
+font-weight: 500;
+line-height: normal;
+
+`;
+
+const Diet = styled(PurposeButton)``;
+const Muscle = styled(PurposeButton)``;
+const Stress = styled(PurposeButton)``;
+const Hobby = styled(PurposeButton)``;
 
 const SliderContainer = styled.div`
   width: 60rem;
@@ -377,7 +360,7 @@ const StyledSlider = styled(ReactSlider)`
   height: 0.5rem;
   background: var(--Main-Color, #6BA6FF);
   position: relative;
-   cursor: pointer;
+  cursor: pointer;
 `;
 
 const StyledThumb = styled.div`
@@ -405,19 +388,17 @@ const Label = styled.div`
   font-size: 2.6rem;
   font-weight: 700;
   line-height: normal;
-    transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
 `;
 
 const labels = ['병아리', '초급', '중급', '고급', '전문가'];
 
-const SliderWithLabels = () => {
-  const [value, setValue] = useState(1);
-
+const SliderWithLabels = ({ value, onChange }) => {
   return (
     <SliderContainer>
       <StyledSlider
         value={value}
-        onChange={setValue}
+        onChange={onChange}
         min={1}
         max={5}
         renderThumb={(props, state) => <StyledThumb {...props} />}
@@ -433,91 +414,162 @@ const SliderWithLabels = () => {
   );
 };
 
-const ModalForm = ({ isOpen, onClose ,data }) => {
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+const ModalForm = ({ isOpen, onClose, data }) => {
+  // 상단에 표시할 튜터 정보
   const tutordetail = data;
+
+  // 튜터에게 보낼 신청서 정보
+  const [title, setTitle] = useState('');
+  const [purpose, setPurpose] = useState([]);
+  const [intensity, setIntensity] = useState(1);
+  const [memo, setMemo] = useState('');
+  const [imgFile, setImgFile] = useState(null);
+
+  // 유저 여기선 userid=1 정보
+  const [userInfo, setUserInfo] = useState({
+    name: '',
+    gender: '',
+    age: '',
+    location: '',
+  });
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try {
+        const response = await axios.get(`${BASE_URL}/users/1`);
+        setUserInfo(response.data.data);
+      } catch (error) {
+        console.error('userid=1 정보 불러오는데 실패:', error);
+      }
+    };
+    fetchUserInfo();
+  }, []);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append('title', title);
+    formData.append('purpose', purpose.join(','));
+    formData.append('intensity', intensity);
+    formData.append('memo', memo);
+    if (imgFile) {
+      formData.append('imgUrl', imgFile, imgFile.name);
+    }
+
+    try {
+      const response = await axios.post(`${BASE_URL}/apps/1/${tutordetail.tutorId}`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      alert('신청서가 접수되었습니다!');
+      setTitle('');
+      setPurpose([]);
+      setIntensity(1);
+      setMemo('');
+      setImgFile(null);
+      onClose(); // 폼 제출 후 모달 닫기
+    } catch (error) {
+      alert(`폼 제출에 실패했습니다: ${error.response.data.detail}`);
+    }
+  };
+
+  const handlePurposeClick = (purposeType) => {
+    setPurpose((prev) =>
+      prev.includes(purposeType) ? prev.filter((p) => p !== purposeType) : [...prev, purposeType]
+    );
+  };
+
+  const isSelected = (purposeType) => purpose.includes(purposeType);
+
   return (
     <>
-      <Overlay isOpen={isOpen} onClick={onClose} />
-      <ModalContainer isOpen={isOpen}>
-        <ModalContent>
-          <button onClick={onClose} style={{ position: 'absolute', top: '6.9rem', left: '7.1rem' ,background:'white',border:'none'}}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40" fill="none">
-              <path d="M1.46447 1.46447C3.41709 -0.488155 6.58291 -0.488155 8.53553 1.46447L20 12.9289L31.4645 1.46447C33.4171 -0.488155 36.5829 -0.488155 38.5355 1.46447C40.4882 3.41709 40.4882 6.58291 38.5355 8.53553L27.0711 20L38.5355 31.4645C40.4882 33.4171 40.4882 36.5829 38.5355 38.5355C36.5829 40.4882 33.4171 40.4882 31.4645 38.5355L20 27.0711L8.53553 38.5355C6.58291 40.4882 3.41709 40.4882 1.46447 38.5355C-0.488155 36.5829 -0.488155 33.4171 1.46447 31.4645L12.9289 20L1.46447 8.53553C-0.488155 6.58291 -0.488155 3.41709 1.46447 1.46447Z" fill="#D9D9D9"/>
-            </svg>
-          </button>
-          <button className='signupformbtn' onClick={onClose} style={{ position: 'absolute', top: '6.9rem', right: '7.1rem' ,width: '14rem', height:'5.3rem',  gap:'1rem', justifyContent:'center', alignItems:'center', background:'#6BA6FF' , color: 'white'}}>
-            신청하기
-          </button>
-          <TutorThumbnail>
-            <TutorImg $imgurl={tutordetail.imgUrl}/>
-            <div>
-              <div>{tutordetail.name} / {tutordetail.sportName}
-                <Space />
-                {Array.from({ length: tutordetail.total_review_score}, (_, index) => (
-                  <RatingImage key={index} src={stars} alt="stars" />
-                ))} ({tutordetail.total_review_count})
+      <form onSubmit={handleSubmit}>
+        <Overlay isOpen={isOpen} onClick={onClose} />
+        <ModalContainer isOpen={isOpen}>
+          <ModalContent>
+            <button onClick={onClose} style={{ position: 'absolute', top: '6.9rem', left: '7.1rem', background: 'white', border: 'none' }}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40" fill="none">
+                <path d="M1.46447 1.46447C3.41709 -0.488155 6.58291 -0.488155 8.53553 1.46447L20 12.9289L31.4645 1.46447C33.4171 -0.488155 36.5829 -0.488155 38.5355 1.46447C40.4882 3.41709 40.4882 6.58291 38.5355 8.53553L27.0711 20L38.5355 31.4645C40.4882 33.4171 40.4882 36.5829 38.5355 38.5355C36.5829 40.4882 33.4171 40.4882 31.4645 38.5355L20 27.0711L8.53553 38.5355C6.58291 40.4882 3.41709 40.4882 1.46447 38.5355C-0.488155 36.5829 -0.488155 33.4171 1.46447 31.4645L12.9289 20L1.46447 8.53553C-0.488155 6.58291 -0.488155 3.41709 1.46447 1.46447Z" fill="#D9D9D9" />
+              </svg>
+            </button>
+            <button type="submit" className="signupformbtn" style={{ position: 'absolute', top: '6.9rem', right: '7.1rem', width: '14rem', height: '5.3rem', gap: '1rem', justifyContent: 'center', alignItems: 'center', background: '#6BA6FF', color: 'white' }}>
+              신청하기
+            </button>
+            <TutorThumbnail>
+              <TutorImg $imgurl={tutordetail.imgUrl} />
+              <div>
+                <div>{tutordetail.name} / {tutordetail.sportName}
+                  <Space />
+                  {Array.from({ length: tutordetail.total_review_score }, (_, index) => (
+                    <RatingImage key={index} src={stars} alt="stars" />
+                  ))} ({tutordetail.total_review_count})
+                </div>
+                <div>{tutordetail.intro}</div>
+                <div>{tutordetail.price}</div>
               </div>
-              <div>{tutordetail.intro}</div>
-              <div>{tutordetail.price}</div>
-            </div>
-          </TutorThumbnail>
-          <ModalTitle>
-            <div>제목</div>
-            <div></div>
-            <div>여기제목</div>
-          </ModalTitle>
-          <TutorFirstLine>
+            </TutorThumbnail>
+            <ModalTitle>
+              <div>제목</div>
+              <div></div>
+              <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="제목" />
+            </ModalTitle>
+            <TutorFirstLine>
               <svg xmlns="http://www.w3.org/2000/svg" width="191" height="191" viewBox="0 0 191 191" fill="none">
                 <circle cx="95.5" cy="95.5" r="95.5" fill="#F2F3F5" />
                 <g transform="translate(39 39)">
                   <svg xmlns="http://www.w3.org/2000/svg" width="113" height="113" viewBox="0 0 113 113" fill="none">
-                    <path d="M9.9375 112.375C9.9375 112.375 0.625 112.375 0.625 103.062C0.625 93.75 9.9375 65.8125 56.5 65.8125C103.062 65.8125 112.375 93.75 112.375 103.062C112.375 112.375 103.062 112.375 103.062 112.375H9.9375Z" fill="#A6A6A6"/>
-                    <path d="M56.5 56.5C71.9295 56.5 84.4375 43.992 84.4375 28.5625C84.4375 13.133 71.9295 0.625 56.5 0.625C41.0705 0.625 28.5625 13.133 28.5625 28.5625C28.5625 43.992 41.0705 56.5 56.5 56.5Z" fill="#A6A6A6"/>
+                    <path d="M9.9375 112.375C9.9375 112.375 0.625 112.375 0.625 103.062C0.625 93.75 9.9375 65.8125 56.5 65.8125C103.062 65.8125 112.375 93.75 112.375 103.062C112.375 112.375 103.062 112.375 103.062 112.375H9.9375Z" fill="#A6A6A6" />
+                    <path d="M56.5 56.5C71.9295 56.5 84.4375 43.992 84.4375 28.5625C84.4375 13.133 71.9295 0.625 56.5 0.625C41.0705 0.625 28.5625 13.133 28.5625 28.5625C28.5625 43.992 41.0705 56.5 56.5 56.5Z" fill="#A6A6A6" />
                   </svg>
                 </g>
               </svg>
-            <div className='infos'>
-              <InfoRow>
-                <div>닉네임</div>
-                <div></div>
-                <div>여기닉네임</div>
-              </InfoRow>
-              <InfoRow>
-                <div>성별</div>
-                <div></div>
-                <div>여기성별</div>
-              </InfoRow>
-              <InfoRow>
-                <div>나이</div>
-                <div></div>
-                <div>여기나이</div>
-              </InfoRow>
-              <InfoRow>
-                <div>지역</div>
-                <div></div>
-                <div>여기지역</div>
-              </InfoRow>
-            </div>
-          </TutorFirstLine>
-          <Purpose>
-            <h3>운동 목적</h3>
-            <Btns>
-              <Diet>다이어트</Diet>
-              <Muscle>근력향상</Muscle>
-              <Stress>스트레스 해소</Stress>
-              <Hobby>취미활동</Hobby>
-            </Btns>
-          </Purpose>
-          <IntensityPreference>
-            <h3>운동 선호 강도</h3>
-            <SliderWithLabels/>
-          </IntensityPreference>
-          <MessageToTutor>
-            <h3>튜터에게 전할 말</h3>
-            <textarea />
-          </MessageToTutor>
-        </ModalContent>
-      </ModalContainer>
+              <div className="infos">
+                <InfoRow>
+                  <div>닉네임</div>
+                  <div></div>
+                  <div>{userInfo.name}</div>
+                </InfoRow>
+                <InfoRow>
+                  <div>성별</div>
+                  <div></div>
+                  <div>{userInfo.gender}</div>
+                </InfoRow>
+                <InfoRow>
+                  <div>나이</div>
+                  <div></div>
+                  <div>{userInfo.age}</div>
+                </InfoRow>
+                <InfoRow>
+                  <div>지역</div>
+                  <div></div>
+                  <div>{userInfo.location}</div>
+                </InfoRow>
+              </div>
+            </TutorFirstLine>
+            <Purpose>
+              <h3>운동 목적</h3>
+              <Btns>
+                <Diet onClick={() => handlePurposeClick('다이어트')} isSelected={isSelected('다이어트')}>다이어트</Diet>
+                <Muscle onClick={() => handlePurposeClick('근력향상')} isSelected={isSelected('근력향상')}>근력향상</Muscle>
+                <Stress onClick={() => handlePurposeClick('스트레스 해소')} isSelected={isSelected('스트레스 해소')}>스트레스 해소</Stress>
+                <Hobby onClick={() => handlePurposeClick('취미활동')} isSelected={isSelected('취미활동')}>취미활동</Hobby>
+              </Btns>
+            </Purpose>
+            <IntensityPreference>
+              <h3>운동 선호 강도</h3>
+              <SliderWithLabels value={intensity} onChange={setIntensity} />
+            </IntensityPreference>
+            <MessageToTutor>
+              <h3>튜터에게 전할 말</h3>
+              <textarea value={memo} onChange={(e) => setMemo(e.target.value)} placeholder="튜터에게 전할 말을 적어주세요!"></textarea>
+            </MessageToTutor>
+          </ModalContent>
+        </ModalContainer>
+      </form>
     </>
   );
 };
